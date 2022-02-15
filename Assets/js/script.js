51d15d878108cb4a8bddc815e95a279f3078989f
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-02-15 01:24:15 -0600

    refactor header

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -12,3 +12,20 @@ testEl.addEventListener("mouseleave", function( event ) {
     $("#back").hide(); 
 });
   
+
+// GAME PLAN!!
+
+/*
+Refactor navbar/title styling - should take 100% of spacing
+- Same with recent projects
+- figure out flexbox
+Highlighted content is now one section
+- flex it 100%, figure out the spacing for the card
+- transition works with jquery: keep it simple!
+other projects (content) is now one section
+- flex it 100%, figure out the spacing
+- keep the design the same for each card, so the HTML can be adjusted, but cards stay the same
+    - possibly use persentages instead of pixels
+Will need to revisit media queries
+About me and down is fine.
+*/
