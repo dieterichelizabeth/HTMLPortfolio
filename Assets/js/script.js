1a6608c8d8b1db2688afaa1f86b77c413af5914d
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-02-25 16:05:20 -0600

    fix back spacing on cards

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -22,34 +22,21 @@ featuredEl.addEventListener("mouseleave", function() {
   
 
 
-// contentCardEl.addEventListener("mouseover", function() {
-//     console.log("flipped!")
-//     $("#front").hide();     
-//     document.getElementById("back").style.visibility = "visible";
-// })
-
-// contentCardEl.addEventListener("mouseleave", function() {
-//     $("#front").show();
-//     document.getElementById("back").style.visibility = "hidden";
-// });
-
-
 
 // GAME PLAN!!
 
 /*
 Done: Refactor navbar/title styling - should take 100% of spacing
-Done: - Same with recent projects
+Done:  Same with recent projects
 Done: Highlighted content is now one section
-Done: - flex it 100%, figure out the spacing for the card
+Done: -flex it 100%, figure out the spacing for the card
 Add github links/further card details
-- transition works with jquery: keep it simple!
+Done: transition works with jquery: keep it simple!
 other projects (content) is now one section
-- flex it 100%, figure out the spacing
-- keep the design the same for each card, so the HTML can be adjusted, but cards stay the same
-    - possibly use persentages instead of pixels
+Done: flex it 100%, figure out the spacing
+Done: keep the design the same for each card, so the HTML can be adjusted, but cards stay the same
 Will need to revisit media queries
     - review needed changes before re-linking
-Done: About me and down is fine.
 Add delay on javascript eventListener
+create function to count cards! For some reason it doesn't apply to multiple cards at once...
 */
